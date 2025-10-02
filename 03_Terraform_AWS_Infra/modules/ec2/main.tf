0f35885d14103a592c63406806fe8a68e133b02b
Mahdi Mabrouk <36667464+MahdiMAB@users.noreply.github.com>
2025-10-02 17:33:38 +0200

    Update main.tf

--- 03_Terraform_AWS_Infra/modules/ec2/main.tf
+++ 03_Terraform_AWS_Infra/modules/ec2/main.tf
@@ -52,7 +52,7 @@ resource "aws_security_group" "web_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Limitez l'accès SSH pour la production
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 
   egress {
